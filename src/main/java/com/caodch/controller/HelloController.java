968702a4c2121bdf0750d57166c8e19de98da738
caodch <496561470@qq.com>
2022-04-14 09:17:28 +0800

    main commit 2

--- src/main/java/com/caodch/controller/HelloController.java
+++ src/main/java/com/caodch/controller/HelloController.java
@@ -4,6 +4,7 @@ import com.caodch.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,7 @@ public class HelloController {
      * @return
      */
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String getHello(){
+    public String getHello(@RequestParam String name){
 
         return "Hello World";
     }
